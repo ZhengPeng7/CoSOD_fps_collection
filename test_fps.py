27638a2caa3cafbd41b7e329d7442d0b7e4fecbd
ZhengPeng7 <zhengpeng0108@gmail.com>
2024-05-21 00:09:05 +0800

    Adapt default setting for CoADNet and GCAGC.

--- test_fps.py
+++ test_fps.py
@@ -9,7 +9,7 @@ from torch.utils import data
 from torchvision import transforms
 
 
-BATCH_SIZE = [8, 5][1]  # For data-random, set to any value. For data-CoCA, set to 5 for CoADNet and GCAGC only.
+BATCH_SIZE = [8, 5][0]  # For data-random, set to any value. For data-CoCA, set to 5 for CoADNet and GCAGC only.
 
 def test_fps(model, model_name, size=256, data=['random', 'CoCA'], batch_size=2):
     # `batch size == 'all'` (not used) means using all images in one group. 

b77a0c2afdf0e5cd26184c2f9528deee985f9213
ZhengPeng7 <zhengpeng0108@gmail.com>
2024-05-21 20:19:15 +0800

    Adapt codes to skip running of CoADNet and GCAGC on testing whole CoCA as batch size of them was fixed to 5.

--- main.py
+++ main.py
@@ -22,13 +22,16 @@ def main():
     repeat_time = 1    # > 1 may lead to reading cache for faster running, which is not fair.
     for data in args.data.split(','):
         for model_name in args.models.split(','):
+            real_batch_size = BATCH_SIZE if data == 'random' else 'all'     # Only work when data == 'random'.
+            if data == 'CoCA' and model_name not in ['CoADNet', 'GCAGC']:
+                print('Model {} on data {} cannot deal with data with batch size {}. Please Change the BATCH_SIZE in `test_fps.py` to 5 to have a test on them for CoCA.'.format(model_name, data, real_batch_size))
+                continue
             model = eval(model_name+'()')
             time_per_frame_lst = []
             for _ in range(repeat_time):
                 time_per_frame = test_fps(model, model_name, size=256, data=data, batch_size=BATCH_SIZE)
                 time_per_frame_lst.append(time_per_frame)
             time_per_frame = sum(time_per_frame_lst) / repeat_time
-            real_batch_size = BATCH_SIZE if data == 'random' else ('all' if model_name not in ['CoADNet', 'GCAGC'] else 5)
             print('Model {} on data {} with batch size {}, running time {:.6f} s, FPS = {:.4f}.'.format(model_name, data, real_batch_size, time_per_frame, 1 / time_per_frame))
     return sum(time_per_frame_lst) / repeat_time
 
